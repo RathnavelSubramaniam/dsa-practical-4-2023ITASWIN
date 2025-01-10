a292663e2df4e67a7b1c8f6ac9b2bf786684aa4c
Aswin <139450941+2023ITASWIN@users.noreply.github.com>
2025-01-10 08:46:46 +0000

    Done 4

--- Array_Insertion.java
+++ Array_Insertion.java
@@ -1,44 +1,43 @@
 import java.util.Scanner;
 
 public class Array_Insertion {
-
     public static void main(String[] args) {
-         int len,p,e;
+        int len, p, e;
         System.out.println("ARRAY OPERATION");
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter the length of the Array");
-        len=sc.nextInt();
-        int arr[]=new int[len+1];
-        System.out.println("Enter "+len+" element(s) to insert");
-        for(int i=0;i<len;i++)
-        {
-            arr[i]=sc.nextInt();
+        len = sc.nextInt();
+        int arr[] = new int[len + 1];
+
+        System.out.println("Enter " + len + " element(s) to insert");
+        for (int i = 0; i < len; i++) {
+            arr[i] = sc.nextInt();
         }
+
         System.out.println("Enter the position where you want to insert:");
-        p=sc.nextInt();
+        p = sc.nextInt();
         System.out.println("Enter the element to be inserted");
-        e=sc.nextInt();
-        arrayInsert(arr,p,e,len);
+        e = sc.nextInt();
+
+        arrayInsert(arr, p, e, len);
+
+        sc.close();
     }
-    
-      static void arrayInsert(int[] arr, int p, int e, int len) {
-        if (p < 0 || p > len) {
-            System.out.println("Invalid position");
-            return;
+
+    static void arrayInsert(int arr[], int p, int e, int len) {
+        for (int i = len - 1; i >= p - 1; i--) {
+            arr[i + 1] = arr[i];
         }
-    
-        for (int i = len; i > p; i--) {
-            arr[i] = arr[i - 1];
-        }
-        
-        arr[p] = e;
-        System.out.println("Array after insertion:");
-        for (int i = 0; i <= len; i++) {
-            System.out.print(arr[i] + " ");
+
+        arr[p - 1] = e;
+
+        System.out.println("After Inserting");
+        System.out.println("Length of the array: " + (len + 1));
+
+        for (int i = 0; i < len + 1; i++) {
+            System.out.println("arr[" + i + "]="+arr[i]);
         }
     }
 }
-    
-
-
 
