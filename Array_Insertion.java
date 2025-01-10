38854ce8a07d2769953386f861689a27c0b5d645
Aswin <139450941+2023ITASWIN@users.noreply.github.com>
2025-01-10 08:42:46 +0000

    done 4

--- Array_Insertion.java
+++ Array_Insertion.java
@@ -20,8 +20,25 @@ public class Array_Insertion {
         e=sc.nextInt();
         arrayInsert(arr,p,e,len);
     }
-    static void arrayInsert()
-    {
-    //TYPE OF CODE HERE
+    
+      static void arrayInsert(int[] arr, int p, int e, int len) {
+        if (p < 0 || p > len) {
+            System.out.println("Invalid position");
+            return;
+        }
+    
+        for (int i = len; i > p; i--) {
+            arr[i] = arr[i - 1];
+        }
+        
+        arr[p] = e;
+        System.out.println("Array after insertion:");
+        for (int i = 0; i <= len; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
+    
+
+
+
